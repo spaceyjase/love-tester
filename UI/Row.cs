201a96adce6b1ecaf58549eb54b7eb57721f27ff
Jason Adams <julietmikealpha@gmail.com>
2021-02-13 17:38:40 +0000

    Initial bulb configuration...

--- UI/Row.cs
+++ UI/Row.cs
@@ -1,16 +1,27 @@
 using Godot;
 using System;
+using System.Xml;
 
 public class Row : HBoxContainer
 {
   [Export] private string description;
+  [Export] private Color bulbColour;
 
   private Label Description => GetNode<Label>("Description");
+  private ColorRect ColorRect => GetNode<ColorRect>("CenterContainer/ColorRect");
+  private static readonly Vector2 colourSize = new Vector2(64, 64);
 
   public override void _Ready()
   {
     base._Ready();
 
+    ConfigureRow();
+  }
+
+  private void ConfigureRow()
+  {
     Description.Text = description;
+    ColorRect.Color = bulbColour;
+    //ColorRect.Visible = false;
   }
 }
