ae244943ff58318ed06d0e43f3fa606846483e34
Jason Adams <julietmikealpha@gmail.com>
2021-02-15 09:59:44 +0000

    Better bulbs

--- UI/Row.cs
+++ UI/Row.cs
@@ -9,6 +9,7 @@ public class Row : HBoxContainer
 
   private Label Description => GetNode<Label>("Description");
   private ColorRect ColorRect => GetNode<ColorRect>("CenterContainer/ColorRect");
+  private Light2D Light => GetNode<Light2D>("CenterContainer/Light2D");
   private static readonly Vector2 colourSize = new Vector2(64, 64);
 
   public override void _Ready()
@@ -21,7 +22,9 @@ public class Row : HBoxContainer
   private void ConfigureRow()
   {
     Description.Text = description;
-    ColorRect.Color = bulbColour;
-    //ColorRect.Visible = false;
+    var backgroundColor = bulbColour;
+    backgroundColor.a = 164f;
+    ColorRect.Color = backgroundColor;
+    Light.Color = bulbColour;
   }
 }
