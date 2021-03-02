292b4ffedc3e51c4747d6d58dad5e47bb32b06fe
Jason Adams <julietmikealpha@gmail.com>
2021-03-02 17:21:11 +0000

    Remove custom buttons, use engine signals.

--- UI/Button.cs
+++ UI/Button.cs
@@ -5,39 +5,9 @@ namespace LoveTester.UI
   public class Button : TextureButton
   {
     [Signal] private delegate void ButtonPressed(string buttonName);
-    
-    [Export] private int downOffset = 3;
 
-    private Label buttonLabel;
-    private Vector2 buttonUpPosition;
-    private Vector2 buttonDownPosition;
-  
-    public override void _Ready()
+    public void OnButtonPressed()
     {
-      base._Ready();
-
-      foreach (var child in GetChildren())
-      {
-        if (!(child is Label childLabel)) continue;
-      
-        buttonLabel = childLabel;
-        buttonUpPosition = buttonLabel.RectPosition;
-        
-        buttonDownPosition = buttonUpPosition;
-        buttonDownPosition.y = buttonUpPosition.y + downOffset;
-      
-        break;
-      }
-    }
-
-    public void OnButtonDown()
-    {
-      buttonLabel.RectPosition = buttonDownPosition;
-    }
-  
-    public void OnButtonUp()
-    {
-      buttonLabel.RectPosition = buttonUpPosition;
       EmitSignal(nameof(ButtonPressed), Name);
     }
   }
